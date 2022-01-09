c71b6a15d9dfae1809dcb7af0009e49d7796c0d9
unknown <anaraquelmelo1@gmail.com>
2022-01-09 11:02:14 +0000

    Get, Get por ID e Insert a funcionar

--- routes/filmes.js
+++ routes/filmes.js
@@ -1,32 +1,67 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const router = express.Router();
+const mysql = require('../mysql').pool;
+
+const jsonParser = bodyParser.json();
 
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Utilização do GET em uma rota de filmes'
-    });
-});
 
-router.post('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Utilização do POST em uma rota de filmes'
+    mysql.getConnection((error, conn) => {
+        if (error) {
+            return res.status(500).send({ error: error });
+        }
+        conn.query(
+            'SELECT * FROM filmes;',
+            (error, result, fields) => {
+                if (error) {
+                    return res.status(500).send({ error: error });
+                }
+                return res.status(200).send({ resultado: result });
+            }
+        )
     });
+
 });
 
 router.get('/:id_filme', (req, res, next) => {
-    const id = req.params.id_filmes
-    res.status(200).send({
-        mensagem: 'ID',
-        id_filmes : id
+    const id = req.params.id_filme;
 
+    mysql.getConnection((error, conn) => {
+        if (error) {
+            return res.status(500).send({ error: error });
+        }
+        conn.query(
+            'SELECT * FROM filmes WHERE filme_id = ?;', [id],
+            (error, result, fields) => {
+                if (error) {
+                    return res.status(500).send({ error: error });
+                }
+                return res.status(200).send({ resultado: result });
+            }
+        )
     });
 });
 
-router.patch('/', (req, res, next) => {
-    res.status(201).send({
-        mensagem: 'Utilização do PATCH em uma rota de filmes'
+router.post('/', jsonParser, (req, res, next) => {    
+    mysql.getConnection((error, conn) => {
+        if (error) {
+            return res.status(500).send({ error: error });
+        }
+        conn.query(
+            'INSERT INTO filmes (filme_id, title, release_year, language, lenght, rating) VALUES (?, ?, ?, ?, ?, ?);', [req.body.filme_id, req.body.title, req.body.release_year, req.body.language, req.body.lenght, req.body.rating],
+            (error, result, fields) => {
+                conn.release();
+                if (error) {
+                    return res.status(500).send({ error: error, response: null });
+                }
+                return res.status(201).send({ mensagem: "Filme inserido com sucesso" });
+            }
+        )
     });
+
 });
+
 
 router.delete('/', (req, res, next) => {
     res.status(201).send({
